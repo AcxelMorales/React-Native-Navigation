1a247b56d47013e2cdf0de6ebeefac68d9c2a630
Acxel Morales <acxelmorales97@gmail.com>
2023-02-11 22:31:18 -0600

    Se agregaron mejoras visuales

--- src/navigator/BottomTabsNavigator.tsx
+++ src/navigator/BottomTabsNavigator.tsx
@@ -1,18 +1,66 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
 
 import Tab1Screen from '../screens/Tab1Screen';
 import Tab2Screen from '../screens/Tab2Screen';
-import Tab3Screen from '../screens/Tab3Screen';
+import {StackNavigator} from './StackNavigator';
+
+import {colors} from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabsNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1" component={Tab1Screen} />
-      <Tab.Screen name="Tab2" component={Tab2Screen} />
-      <Tab.Screen name="Tab3" component={Tab3Screen} />
+    <Tab.Navigator
+      screenOptions={({route: {name}}) => ({
+        tabBarActiveTintColor: colors.primary,
+        tabBarStyle: {
+          borderTopColor: colors.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontSize: 13,
+        },
+        tabBarIcon: ({color}) => {
+          let iconName: string = '';
+
+          switch (name) {
+            case 'Tab1':
+              iconName = 'T1';
+              break;
+            case 'Tab2':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'ST';
+              break;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+      })}
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+    >
+      <Tab.Screen
+        name="Tab1"
+        options={{title: 'Tab1'}}
+        component={Tab1Screen}
+      />
+      <Tab.Screen
+        name="Tab2"
+        options={{title: 'Tab2'}}
+        component={Tab2Screen}
+      />
+      <Tab.Screen
+        name="StackNavigator"
+        options={{title: 'Stack'}}
+        component={StackNavigator}
+      />
     </Tab.Navigator>
   );
 };
